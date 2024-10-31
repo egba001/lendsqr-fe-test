db19338773ab68c116e92478fb52879345da8f16
Egba Nnaemeka <emekason128@gmail.com>
2024-10-31 07:25:29 +0100

    Completed login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,139 @@
 'use client'
-import { useState } from 'react'
+import Link from 'next/link'
+import React, { FormHTMLAttributes, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import logo from '@/assets/image/icons/logo.svg'
+import illustration from '@/assets/image/icons/illustration.svg'
+import Image from 'next/image'
+import styles from '@/assets/styles/login.module.scss'
+import localFont from 'next/font/local'
+
+const avenirBold = localFont({
+    src: '../assets/font/AvenirNextLTPro-Bold.otf',
+    display: 'swap',
+})
+
+const avenirRegular = localFont({
+    src: '../assets/font/AvenirNextLTPro-Regular.otf',
+    display: 'swap',
+})
 
 export default function Home() {
-    // Interfae for form data
+    // Interface for form data
     interface FormData {
         email: string
         password: string
     }
 
+    const router = useRouter()
+
+    // State to hold login data
     const [formData, setFormData] = useState<FormData>({
         email: '',
         password: '',
     })
 
+    // Boolean state to determine password display
+    const [showPassword, setShowPassword] = useState(false)
+
+    // Change handler function for input component
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = e.target
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+
+    // Function to handle form submission
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault()
+        router.push('/dashboard/users')
+    }
+
     return (
-        <div className="login-container">
+        <div className={styles.login_container}>
             {/* Illustration section */}
-            <div></div>
+            <div className={styles.illustration_container}>
+                <div className={styles.image_group}>
+                    <Image
+                        src={logo}
+                        alt="Lendsqr logo"
+                        width={200}
+                        height={50}
+                    />
+                    <Image
+                        src={illustration}
+                        alt="Illustration"
+                        width={700}
+                        height={250}
+                    />
+                </div>
+            </div>
 
             {/* Login Section */}
-            <div className="login-section">
-                <div>
-                    <h1 className="heading-one">Welcome!</h1>
-                    <p className="heading-two">Enter Details to continue</p>
+            <div className={styles.login_section}>
+                <div className={styles.form_container}>
+                    <h1
+                        className={`${styles.main_heading} ${avenirBold.className}`}
+                    >
+                        Welcome!
+                    </h1>
+                    <p
+                        className={`${avenirRegular.className} ${styles.sub_heading}`}
+                    >
+                        Enter Details to continue
+                    </p>
 
-                    <input type="email" name="email" value={FormData.value} />
+                    <form
+                        className={avenirRegular.className}
+                        onSubmit={handleSubmit}
+                    >
+                        <div className={styles.inputs_container}>
+                            <input
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                placeholder="Email"
+                                onChange={onInputChange}
+                                required
+                                className={styles.input_styling}
+                            />
+
+                            <div className={styles.password_input}>
+                                <input
+                                    type={showPassword ? 'text' : 'password'}
+                                    name="password"
+                                    placeholder="Password"
+                                    onChange={onInputChange}
+                                    required
+                                    value={formData.password}
+                                    className={styles.input_styling}
+                                />
+                                <button
+                                    type="button"
+                                    onClick={() =>
+                                        setShowPassword(!showPassword)
+                                    }
+                                    className={
+                                        styles.password_visibility_button
+                                    }
+                                >
+                                    {showPassword ? 'Hide' : 'Show'}
+                                </button>
+                            </div>
+                        </div>
+                        <Link href="#" className={styles.fg_password}>
+                            Forgot Password?
+                        </Link>
+                        <button
+                            className={styles.login_button}
+                            type="submit"
+                            disabled={!formData.email || !formData.password}
+                        >
+                            Log in
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
 import briefcase from '@/assets/image/icons/briefcase.svg'
 import arrowDown from '@/assets/image/icons/arrow_down.svg'
-import { customersNavigations } from '@/utils/constants'
+import {
+    customersNavigations,
+    businessesNavigation,
+    settingsNavigation,
+} from '@/utils/constants'
 import Link from 'next/link'
+import styles from '@/assets/styles/sidebar.module.scss'
 
 export default function Sidebar() {
     return (
@@ -19,11 +24,32 @@ export default function Sidebar() {
                     <span>Dashboard</span>
                 </button>
 
+                {/* Cutomers Navigation Menu */}
                 <p className="nav-heading">Customers</p>
-                <ul>
+                <ul className={styles.sidebar_navigation}>
                     {customersNavigations.map((nav, id) => (
-                        <li>
-                            <Link href="/"></Link>
+                        <li key={id}>
+                            <Link href="/">{nav.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+
+                {/* Businesses Navigation Menu */}
+                <p className="nav-heading">Businesses</p>
+                <ul className={styles.sidebar_navigation}>
+                    {businessesNavigation.map((nav, id) => (
+                        <li key={id}>
+                            <Link href="/">{nav.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+
+                {/* Settings Navigation Menu */}
+                <p className="nav-heading">Businesses</p>
+                <ul className={styles.sidebar_navigation}>
+                    {settingsNavigation.map((nav, id) => (
+                        <li key={id}>
+                            <Link href="/">{nav.title}</Link>
                         </li>
                     ))}
                 </ul>
